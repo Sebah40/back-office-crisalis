ce08dec2da3ec22a70bff188e88a24246d8e8c0f
sebah40 <sebastian.haoys@gmail.com>
2023-10-08 16:04:42 -0300

    Se agregó el método getAll. Devuelve un string de los nombres de usuario, mails y rol. Se agregó CrossOrigin para probar métodos con Angular desde localhost:4200.

--- backend/src/main/java/com/orange/Crisalis/security/Entity/Usuario.java
+++ backend/src/main/java/com/orange/Crisalis/security/Entity/Usuario.java
@@ -13,6 +13,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotNull;
+import com.orange.Crisalis.security.Entity.Rol;
 
 /**
  *

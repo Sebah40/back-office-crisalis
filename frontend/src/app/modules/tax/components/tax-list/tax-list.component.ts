aa13e258714ac72a8b6062305da6afe567b2b65e
Gonzalo Fleitas <psi.gonzalo.fleitas@gmail.com>
2023-11-03 13:00:29 -0300

    refactorizando el front

---
+++ frontend/src/app/modules/tax/components/tax-list/tax-list.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { TaxService } from '../../service/tax.service';
+import { EntityContainerComponent } from '../../../shared/components/entity-container-component/entity-container.component';
+@Component({
+  selector: 'app-tax',
+  templateUrl: './tax-list.component.html',
+  styleUrls: ['./tax-list.component.css'],
+})
+export class TaxListComponent<T extends object> implements OnInit {
+  taxListData: any[] = [];
+  @Input() entities: T[] = [];
+  entityKeys: (keyof T)[] = [];
+  @Input() title: string = '';
+  @Input() isEditable: boolean = false;
+
+  constructor(private taxService: TaxService) {}
+
+  ngOnInit() {
+    this.taxService.getAll().subscribe((data) => {
+      this.taxListData = data;
+    });
+  }
+  ngOnChanges() {
+    if (this.entities && this.entities.length > 0) {
+      this.entityKeys = Object.keys(this.entities[0]) as (keyof T)[];
+    }
+    console.log(this.entities);
+  }
+
+  getData(): any[] {
+    console.log(
+      this.entities.map((item) => this.entityKeys.map((key) => item[key]))
+    );
+    return this.entities.map((item) => this.entityKeys.map((key) => item[key]));
+  }
+  deleteTax(entity: any) {
+    console.log('taxList', entity);
+    const tax: { id: number } = { id: entity.id };
+    console.log('tax', tax);
+    this.taxService.deleteTax(tax.id).subscribe({
+      next: (response: any) => {
+        this.taxService.updateTaxData();
+      },
+      error: (error: any) => {
+        console.log(error);
+      },
+    });
+  }
+}
---
+++ frontend/src/app/modules/user/components/users-list/user-list.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { IUserGet } from '../../model/UserGet.model';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css'],
+})
+export class UserListComponent implements OnInit {
+  public userListData: IUserGet[] = [];
+  public editableValue: boolean = true;
+
+  constructor(public userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.userListData$.subscribe((users) => {
+      this.userListData = users; // Actualiza la lista de usuarios
+    });
+
+    // Inicialmente, obtén la lista de usuarios
+    this.userService.getAll().subscribe((users) => {
+      this.userListData = users;
+    });
+  }
+  deleteUser(entity: any) {
+    const user: { username: string } = { username: entity.username };
+
+    this.userService.delete(user).subscribe({
+      next: (response: any) => {
+        this.userService.updateUserListData();
+        alert(response.mensaje);
+      },
+      error: (error: any) => {
+        console.log(error);
+      },
+    });
+  }
+}

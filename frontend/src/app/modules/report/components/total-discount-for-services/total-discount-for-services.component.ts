81c9b6c80a3159f96f0b3c5eca8e06f684ace502
Gonzalo Fleitas <psi.gonzalo.fleitas@gmail.com>
2023-11-28 13:28:05 -0300

    creando reporte descuentos totales

--- frontend/src/app/modules/report/components/total-discount-for-services/total-discount-for-services.component.ts
+++ frontend/src/app/modules/report/components/total-discount-for-services/total-discount-for-services.component.ts
@@ -1,38 +1,10 @@
-import { Component, ViewChild } from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
-import { MatSort } from '@angular/material/sort';
-import { ActivatedRoute, Router } from '@angular/router';
-
-const DATA: any[] = [
-  {
-    clientId: 1,
-    clientName: 'Gonzalo Fleitas',
-    service: 'Linea celular',
-    discount: 900.0,
-    orderNum: '001',
-  },
-  {
-    clientId: 2,
-    clientName: 'Taxi SRL',
-    service: 'Internet 100MB',
-    discount: 2500.0,
-    orderNum: '043',
-  },
-  {
-    clientId: 3,
-    clientName: 'Pepsi SA',
-    service: 'Netflix',
-    discount: 250.0,
-    orderNum: '021',
-  },
-  {
-    clientId: 4,
-    clientName: 'Coca-Cola',
-    service: 'Internet 200MB',
-    discount: 1200.0,
-    orderNum: '002',
-  },
-];
+import { Component, OnInit } from '@angular/core';
+import { OrderByClient } from '../../DTOs/orderByClientDTO.model';
+import { GroupClient } from '../../DTOs/groupData.model';
+import { ActivatedRoute } from '@angular/router';
+import { DataForTotalDiscount } from '../../DTOs/dataForTotalDiscount.model';
+import { GroupClientForDiscount } from '../../DTOs/groupDataTotalDiscount.model';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-total-discount-for-services',
@@ -40,47 +12,119 @@ const DATA: any[] = [
   styleUrls: ['./total-discount-for-services.component.css'],
 })
 export class TotalDiscountForServicesComponent {
-  dataSource!: any;
-  displayedColumns: string[] = [
-    'clientName',
-    'service',
-    'discount',
-    'orderNum',
-  ];
-  @ViewChild(MatSort) sort!: MatSort;
+  title = 'Informe de descuentos totales en pedidos por cliente';
 
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  data: DataForTotalDiscount[] = [
+    {
+      clientId: 1,
+      client: 'Coca Cola',
+      service: 'Internet 100 MB',
+      orderNum: 23,
+      date: '01-feb',
+      discount: 1200,
+    },
+    {
+      clientId: 1,
+      client: 'Coca Cola',
+      service: 'Internet 100 MB',
+      orderNum: 11,
+      date: '22-feb',
+      discount: 900,
+    },
+    {
+      clientId: 2,
+      client: 'Pepsi',
+      service: 'Internet 100 MB',
+      orderNum: 44,
+      date: '25-feb',
+      discount: 1900,
+    },
+    {
+      clientId: 1,
+      client: 'Coca Cola',
+      service: 'Linea Celular',
+      orderNum: 45,
+      date: '27-feb',
+      discount: 200,
+    },
+  ];
+
+  constructor(private route: ActivatedRoute, private location: Location) {}
 
   ngOnInit(): void {
     this.route.queryParams.subscribe((param) => {
       const clientId = param['clientId'];
-      console.log(clientId);
-      if (clientId === 'null') {
-        this.dataSource = new MatTableDataSource<any>(DATA);
-      } else {
-        this.dataSource = new MatTableDataSource<any>(
-          DATA.filter((e) => e.clientId == clientId)
-        );
+
+      if (clientId !== 'null') {
+        this.data = this.data.filter((element) => element.clientId == clientId);
       }
+      const dateFrom = param['dateFrom'];
+      const dateTo = param['dateTo'];
+      console.log(clientId);
     });
   }
 
-  ngAfterViewInit() {
-    this.dataSource.sort = this.sort;
+  valores(order: OrderByClient): any[] {
+    return Object.values(order);
   }
 
-  getColumnHeader(column: string): string {
-    switch (column) {
-      case 'clientName':
-        return 'Cliente';
-      case 'service':
-        return 'Servicio';
-      case 'orderNum':
-        return 'Número de Pedido';
-      case 'discount':
-        return 'Mayor Descuento';
-      default:
-        return column;
-    }
+  group(orders: DataForTotalDiscount[]): GroupClientForDiscount[] {
+    return orders.reduce(
+      (acc: GroupClientForDiscount[], order: DataForTotalDiscount) => {
+        const clientGroup = acc.find((group) => group.client === order.client);
+
+        if (clientGroup) {
+          const serviceGroup = clientGroup.services.find(
+            (serv) => serv.service === order.service
+          );
+
+          if (serviceGroup) {
+            serviceGroup.totalDiscount += order.discount;
+
+            serviceGroup.items.push({
+              orderNum: order.orderNum,
+              discount: order.discount,
+              date: new Date(order.date),
+            });
+          } else {
+            clientGroup.services.push({
+              service: order.service,
+              totalDiscount: order.discount,
+              items: [
+                {
+                  orderNum: order.orderNum,
+                  date: new Date(order.date),
+                  discount: order.discount,
+                },
+              ],
+            });
+          }
+        } else {
+          acc.push({
+            client: order.client,
+            services: [
+              {
+                service: order.service,
+                totalDiscount: order.discount,
+                items: [
+                  {
+                    orderNum: order.orderNum,
+                    date: new Date(order.date),
+                    discount: order.discount,
+                  },
+                ],
+              },
+            ],
+          });
+        }
+
+        return acc;
+      },
+      []
+    );
+  }
+
+  goBack() {
+    this.location.back();
   }
 }

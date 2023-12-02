278cab7deedaa9066a31839c7ee386674d674034
Gonzalo Fleitas <psi.gonzalo.fleitas@gmail.com>
2023-12-02 20:41:29 -0300

    quitando redirect

--- frontend/src/app/order/order-list/order-list.component.ts
+++ frontend/src/app/order/order-list/order-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import  { OrderDTO } from '../model/order-dto';
+import { OrderDTO } from '../model/order-dto';
 import { OrderService } from '../service/order.service';
 import { RouterModule, Routes, Router } from '@angular/router';
 import Swal from 'sweetalert2';
@@ -7,42 +7,37 @@ import Swal from 'sweetalert2';
 @Component({
   selector: 'app-order-list',
   templateUrl: './order-list.component.html',
-  styleUrls: ['./order-list.component.css']
+  styleUrls: ['./order-list.component.css'],
 })
 export class OrderListComponent implements OnInit {
-
-
-
   //order = new OrderDTO(300, 2, new Date());
-
 
   constructor(private orderService: OrderService, private router: Router) {}
 
   orderList: OrderDTO[] = [];
   loadOrderList(): void {
-      this.orderService.orderList().subscribe(orderList => {
+    this.orderService.orderList().subscribe(
+      (orderList) => {
         this.orderList = orderList;
         console.log(this.orderList);
-      }, err => {
+      },
+      (err) => {
         console.log(err);
-      })
+      }
+    );
   }
 
   delete(id: any): void {
     this.orderService.delete(id).subscribe();
   }
   validate(id: any) {
-    this.orderService.validate(id).subscribe(res => res);
-    this.redirect('order/'+id);
-    Swal.fire(
-      'Órden validada',
-      undefined,
-      'success'
-    );
+    this.orderService.validate(id).subscribe((res) => res);
+
+    Swal.fire('Órden validada', undefined, 'success');
   }
 
   redirect(id: any): void {
-    this.router.navigate(['/'+id]);
+    this.router.navigate(['/' + id]);
   }
 
   ngOnInit(): void {
@@ -53,12 +48,10 @@ export class OrderListComponent implements OnInit {
   entityKeys = ['N° de pedido', 'Responsable', 'Fecha', 'Total'];
   title: string = 'pedidos';
 
-
   entityKeyToRedirect: string = '';
   pathCreateEntity: string = '';
   pathEditEntity: string = '';
   buttonText: string = '';
   isEditable: boolean = false;
   deleteEntity?: (entity: any) => void;
-
 }

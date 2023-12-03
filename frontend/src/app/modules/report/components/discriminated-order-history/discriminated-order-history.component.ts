ba7848a90369f2bba84617a8479605ede011cadd
Gonzalo Fleitas <psi.gonzalo.fleitas@gmail.com>
2023-12-02 22:02:18 -0300

    modificando estilos

--- frontend/src/app/modules/report/components/discriminated-order-history/discriminated-order-history.component.ts
+++ frontend/src/app/modules/report/components/discriminated-order-history/discriminated-order-history.component.ts
@@ -10,105 +10,27 @@ import { SimpleContainerComponent } from 'src/app/modules/shared/components/simp
 @Component({
   selector: 'app-discriminated-order-history',
   templateUrl: './discriminated-order-history.component.html',
-  styleUrls: ['./discriminated-order-history.component.css']
+  styleUrls: ['./discriminated-order-history.component.css'],
 })
 export class DiscriminatedOrderHistoryComponent implements OnInit {
-  title = "Informe de historial de pedidos por cliente"
-  @ViewChild(SimpleContainerComponent) simpleContainer!:SimpleContainerComponent; 
+  title = 'Informe de historial de pedidos por cliente';
+  @ViewChild(SimpleContainerComponent)
+  simpleContainer!: SimpleContainerComponent;
 
-  data:OrderByClient[] = [
-    {
-      clientName: "Coca Cola",
-      sellableGood: "Internet 100 MB",
-      orderID: 23,
-      orderStatus: "Activo",
-      orderDate: "01-feb",
-      quantity: 1,
-      price: 2000,
-      subtotal: 2000,
-      taxes: 2100,
-      total: 10250,
-      discount: 0,
-      clientID: 1,
-      warrantyValue: 0,
-      supportCharge: 0
-    },
-    {
-      clientName: "Coca Cola",
-      sellableGood: "Celular",
-      orderID: 23,
-      orderStatus: "Activo",
-      orderDate: "01-feb",
-      quantity: 2,
-      price: 300,
-      subtotal: 500,
-      taxes: 2100,
-      total: 10250,
-      discount: 0,
-      clientID: 1,
-      warrantyValue: 0,
-      supportCharge: 0
-    },
-    {
-      clientName: "Coca Cola",
-      sellableGood: "Celular",
-      orderID: 34,
-      orderStatus: "Anulado",
-      orderDate: "05-mar",
-      quantity: 1,
-      price: 350,
-      subtotal: 350,
-      taxes: 70,
-      total: 420,
-      discount: 0,
-      clientID: 1,
-      warrantyValue: 0,
-      supportCharge:0
-    },
-    {
-      clientName: "Pepsi S.a.",
-      sellableGood: "Celular",
-      orderID: 25,
-      orderStatus: "Activo",
-      orderDate: "02-feb",
-      quantity: 1,
-      price: 300,
-      subtotal: 300,
-      taxes: 60,
-      total: 420,
-      discount: 0,
-      clientID: 1,
-      warrantyValue: 0,
-      supportCharge:0
-    },
-    {
-      clientName: "Pepsi S.a.",
-      sellableGood: "Internet 200 MB",
-      orderID: 26,
-      orderStatus: "Activo",
-      orderDate: "03-feb",
-      quantity: 1,
-      price: 2500,
-      subtotal: 2500,
-      taxes: 2600,
-      total: 12550,
-      discount: 0,
-      clientID: 1,
-      warrantyValue: 0,
-      supportCharge: 0
-    }
-  ];
-  
-  constructor(private reportService: ReportService,
-    private route: ActivatedRoute, 
+  data: OrderByClient[] = [];
+
+  constructor(
+    private reportService: ReportService,
+    private route: ActivatedRoute,
     private location: Location,
-    private pdfService: PdfService) {}
+    private pdfService: PdfService
+  ) {}
 
   ngOnInit(): void {
     this.route.queryParams.subscribe((param) => {
       const clientId = param['clientId'];
-      const dateFrom = param['dateFrom']
-      const dateTo = param['dateTo']
+      const dateFrom = param['dateFrom'];
+      const dateTo = param['dateTo'];
       this.reportService
         .generateOrderHistory(dateFrom, dateTo, clientId)
         .subscribe({
@@ -127,18 +49,22 @@ export class DiscriminatedOrderHistoryComponent implements OnInit {
     const content = this.simpleContainer.getRootElement();
     this.pdfService.generatePdf(content, 'informe-historial.pdf');
   }
-  
-  valores(order:OrderByClient):any[]{
-    return Object.values(order);    
+
+  valores(order: OrderByClient): any[] {
+    return Object.values(order);
   }
 
-  group(orders:OrderByClient[]):GroupClient[]{
+  group(orders: OrderByClient[]): GroupClient[] {
     return orders.reduce((acc: GroupClient[], order: OrderByClient) => {
-      const clientGroup = acc.find(group => group.client === order.clientName);
-    
+      const clientGroup = acc.find(
+        (group) => group.client === order.clientName
+      );
+
       if (clientGroup) {
-        const goodGroup = clientGroup.items.find(good => good.sellablegood === order.sellableGood);
-    
+        const goodGroup = clientGroup.items.find(
+          (good) => good.sellablegood === order.sellableGood
+        );
+
         if (goodGroup) {
           goodGroup.quantityAccumulator += order.quantity;
           goodGroup.subtotalAccumulator += order.subtotal;
@@ -156,7 +82,7 @@ export class DiscriminatedOrderHistoryComponent implements OnInit {
             total: order.total,
             warrantyValue: order.warrantyValue,
             supportCharge: order.supportCharge,
-            discount: order.discount
+            discount: order.discount,
           });
         } else {
           clientGroup.items.push({
@@ -166,48 +92,54 @@ export class DiscriminatedOrderHistoryComponent implements OnInit {
             totalTaxesAccumulator: order.taxes,
             discountAccumulator: order.discount,
             totalAccumulator: order.total,
-            items: [{
-              orderId: order.orderID,
-              status: order.orderStatus,
-              date: new Date(order.orderDate),
-              quantity: order.quantity,
-              price: order.price,
-              subtotal: order.subtotal,
-              totalTaxes: order.taxes,
-              total: order.total,
-              warrantyValue: order.warrantyValue,
-              supportCharge: order.supportCharge,
-              discount: order.discount
-            }]
+            items: [
+              {
+                orderId: order.orderID,
+                status: order.orderStatus,
+                date: new Date(order.orderDate),
+                quantity: order.quantity,
+                price: order.price,
+                subtotal: order.subtotal,
+                totalTaxes: order.taxes,
+                total: order.total,
+                warrantyValue: order.warrantyValue,
+                supportCharge: order.supportCharge,
+                discount: order.discount,
+              },
+            ],
           });
         }
       } else {
         acc.push({
           client: order.clientName,
-          items: [{
-            sellablegood: order.sellableGood,
-            quantityAccumulator: order.quantity,
-            subtotalAccumulator: order.subtotal,
-            totalTaxesAccumulator: order.taxes,
-            discountAccumulator: order.discount,
-            totalAccumulator: order.total,
-            items: [{
-              orderId: order.orderID,
-              status: order.orderStatus,
-              date: new Date(order.orderDate),
-              quantity: order.quantity,
-              price: order.price,
-              subtotal: order.subtotal,
-              totalTaxes: order.taxes,
-              total: order.total,
-              warrantyValue: order.warrantyValue,
-              supportCharge: order.supportCharge,
-              discount: order.discount
-            }]
-          }]
+          items: [
+            {
+              sellablegood: order.sellableGood,
+              quantityAccumulator: order.quantity,
+              subtotalAccumulator: order.subtotal,
+              totalTaxesAccumulator: order.taxes,
+              discountAccumulator: order.discount,
+              totalAccumulator: order.total,
+              items: [
+                {
+                  orderId: order.orderID,
+                  status: order.orderStatus,
+                  date: new Date(order.orderDate),
+                  quantity: order.quantity,
+                  price: order.price,
+                  subtotal: order.subtotal,
+                  totalTaxes: order.taxes,
+                  total: order.total,
+                  warrantyValue: order.warrantyValue,
+                  supportCharge: order.supportCharge,
+                  discount: order.discount,
+                },
+              ],
+            },
+          ],
         });
       }
-    
+
       return acc;
     }, []);
   }
